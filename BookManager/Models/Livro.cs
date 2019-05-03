d1d98089c81ff133268d6206b156bcbcb527eb64
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2019-05-02 21:00:47 -0300

    Criação da classe livro e incio da cração do menu

---
+++ BookManager/Models/Livro.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookManager.Models
+{
+  public class Livro
+  {
+    public string Nome { get; set; } // = "C#"; 
+    public string Descricao { get; set; }
+    public int Ano { get; set; }
+    public DateTime DataLancamento { get; set; }
+    public float Largura { get; set; }
+    public float Altura { get; set; }
+    public float Profundidade { get; set; }
+    public string Dimensoes { get => $"{Largura} x {Altura} x {Profundidade}"; }
+
+
+    public Livro(string nome)
+    {
+      Nome = nome;
+    }
+  }
+}
--- BookManager/Program.cs
+++ BookManager/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using static System.Console;
+using static System.DateTime;
 
 namespace BookManager
 {
@@ -6,7 +8,49 @@ namespace BookManager
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello World!");
+      Console.WriteLine($"Bookmanag Inciado às {Now.ToShortDateString()}");
+      while (true)
+        ShowMenu();
+    }
+    private static void ShowMenu()
+    {
+      Console.WriteLine("Escolha uma das opções abaixo:");
+      Console.WriteLine("1. Adicionar um novo livro");
+      Console.WriteLine("2. Listar livros cadastrados");
+      Console.WriteLine("3. Remover um livro");
+      string opcao = ReadLine();
+
+      switch (opcao)
+      {
+        case "1":
+          AdicionarNovoLivro();
+          break;
+        case "2":
+          ListarLivros();
+          break;
+        case "3":
+          RemoverLivros();
+          break;
+        default:
+          Console.WriteLine("Opção Invalida");
+
+          break;
+      }
+    }
+
+    private static void RemoverLivros()
+    {
+      
+    }
+
+    private static void ListarLivros()
+    {
+      
+    }
+
+    private static void AdicionarNovoLivro()
+    {
+     
     }
   }
 }
