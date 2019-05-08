b347c5dd101f1ac38711a4b0beaabea85cc429e0
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2019-05-07 23:03:16 -0300

    Implementação dos métodos adicionar livro

--- BookManager/Models/Livro.cs
+++ BookManager/Models/Livro.cs
@@ -10,12 +10,13 @@ namespace BookManager.Models
   {
     public string Nome { get; set; } // = "C#"; 
     public string Descricao { get; set; }
+    public string Editora { get; set; }
     public int Ano { get; set; }
     public DateTime DataLancamento { get; set; }
     public float Largura { get; set; }
     public float Altura { get; set; }
     public float Profundidade { get; set; }
-    public string Dimensoes { get => $"{Largura} x {Altura} x {Profundidade}"; }
+    public string Dimensoes { get => $"{Largura} x {Altura} x {Profundidade} cm"; }
 
 
     public Livro(string nome)
--- BookManager/Program.cs
+++ BookManager/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BookManager.Models;
+using System;
+using System.Collections.Generic;
 using static System.Console;
 using static System.DateTime;
 
@@ -6,10 +8,12 @@ namespace BookManager
 {
   class Program
   {
+    static bool done;
+    static List<Livro> Livros = new List<Livro>();
     static void Main(string[] args)
     {
       Console.WriteLine($"Bookmanag Inciado às {Now.ToShortDateString()}");
-      while (true)
+      while (!done)
         ShowMenu();
     }
     private static void ShowMenu()
@@ -18,6 +22,8 @@ namespace BookManager
       Console.WriteLine("1. Adicionar um novo livro");
       Console.WriteLine("2. Listar livros cadastrados");
       Console.WriteLine("3. Remover um livro");
+      Console.WriteLine("S. Sair");
+
       string opcao = ReadLine();
 
       switch (opcao)
@@ -30,6 +36,9 @@ namespace BookManager
           break;
         case "3":
           RemoverLivros();
+          break;
+        case string o when o == "s" || o == "S":
+          done = true;
           break;
         default:
           Console.WriteLine("Opção Invalida");
@@ -45,12 +54,67 @@ namespace BookManager
 
     private static void ListarLivros()
     {
+      for (int i = 0; i < Livros.Count; i++)
+      {
+        var livro = Livros[i];
+      }
+      foreach (var livro in Livros)
+      {
+        WriteLine(livro.Nome);
+
+      }
       
     }
 
     private static void AdicionarNovoLivro()
     {
-     
+      Console.WriteLine("Digite o nome do livro");
+      var nome = ReadLine();
+
+      Console.WriteLine("Digite o nome do descrição");
+      var descricao = ReadLine();
+
+      Console.WriteLine("Digite a editora do livro");
+      var editora = ReadLine();
+
+      Console.WriteLine("Digite o ano do livro");
+      int ano;
+      while(!int.TryParse(ReadLine(), out ano))
+        WriteLine("Ano inválido digite novamente");
+
+      Console.WriteLine("Digite a data de lcmto do livro (fomato dd/mm/aaaa");
+      DateTime dataLancamento;
+      while (!DateTime.TryParse(ReadLine(), out dataLancamento))
+        WriteLine("Data lcmto inválido digite novamente");
+
+      Console.WriteLine("Digite a largura do livro (cm)");
+      float largura;
+      while (!float.TryParse(ReadLine(), out largura))
+        WriteLine("Largura inválido digite novamente");
+
+      Console.WriteLine("Digite a altura do livro (cm)");
+      float altura;
+      while (!float.TryParse(ReadLine(), out altura))
+        WriteLine("Altura inválido digite novamente");
+
+      Console.WriteLine("Digite a profundidade do livro (cm)");
+      float profundidade;
+      while (!float.TryParse(ReadLine(), out profundidade))
+        WriteLine("Profundidade inválido digite novamente");
+
+      var livro = new Livro(nome)
+      {
+        Descricao = descricao,
+        Editora = editora,
+        Ano = ano,
+        DataLancamento = dataLancamento,
+        Altura = altura,
+        Largura = largura,
+        Profundidade = profundidade
+      };
+
+      Livros.Add(livro);
+
     }
   }
 }
